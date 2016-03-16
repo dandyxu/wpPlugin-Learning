078ffe8e0295e71cb556123fb3386c6f27fb0452
dandyxu <dandyjefferson@gmail.com>
2016-03-16 11:44:24 +0000

    Day 5 Writing a filter

---
+++ wp-content/plugins/content_watermark_plugin.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Content Watermark Plugin
+Plugin URI: http://localhost/wpPlugin-learning/content_watermark
+Description: This plugin will add a "watermark" to content
+Author: Dandy Xu
+Version: 1.0
+Anthor URI: http://wwww.techxu.com
+
+*/
+
+function cwmp_add_content($content){
+	if (is_feed()){
+		return $content . 'Created by Dandy Xu, copyright'. date('Y') . 'all rights reserved';
+	}
+	return $content;
+}
+
+add_filter('the_content', 'cwmp_add_content');
+//remove_filter('the_content','cwmp_add_content');
+
+?>
