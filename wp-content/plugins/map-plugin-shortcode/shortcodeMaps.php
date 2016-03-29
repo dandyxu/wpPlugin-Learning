546e6f5b48d6215d59a62cd717b5ee23d60047a7
dandyxu <dandyjefferson@gmail.com>
2016-03-29 17:48:20 +0100

    Day 8 Using shortcode 1st part in office

---
+++ wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Map plugin using shortcode
+Plugin URI: http://localhost/wpPlugin-learning/map-plugin-shortcode/
+Description: This plugin will get a map of whatever parameter is passed
+Author: Dandy Xu
+Version: 1.0
+Author URI: https://github.com/dandyxu
+*/
+
+function smp_map_it(){
+
+	$addr = "10, The Crescent, O'Connell Street, Limerick";
+}
+?>
