877b35e7c23da987cb46ae6afee3b83576565914
dandyxu <dandyjefferson@gmail.com>
2016-05-04 18:06:46 +0100

    Day 9 Using shortcode 2nd part in office

--- wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
+++ wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
@@ -11,6 +11,14 @@ Author URI: https://github.com/dandyxu
 function smp_map_it(){
 
 	$addr = "10, The Crescent, O'Connell Street, Limerick";
+	$base_map_url = "https://www.google.ie/maps/place/10+The+Crescent,+Limerick/@52.6619208,-8.6303102,15z/data=!4m2!3m1!1s0x485b5c630e53d727:0x1d6c9d51e44f8943?hl=en";
+	?>
+	<h2>Your Location: </h2>
+	<img width="256" height="256" 
+		src="<?php echo $base_map_url . urlencode($addr); ?>" />
+	<?php
 }
+
+add_shortcode ('map-it', 'smp_map_it');
 
 ?>
