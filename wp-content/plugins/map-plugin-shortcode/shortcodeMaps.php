6fe39185862bb668a945d9606eb5a2f31d3ad76c
dandyxu <dandyjefferson@gmail.com>
2016-06-27 16:48:24 +0100

    Day 10 Using shortcode is finally finished, how lazy I am. Keep going, man! You're still weak.

--- wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
+++ wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
@@ -4,7 +4,7 @@ Plugin Name: Map plugin using shortcode
 Plugin URI: http://localhost/wpPlugin-learning/map-plugin-shortcode/
 Description: This plugin will get a map of whatever parameter is passed
 Author: Dandy Xu
-Version: 2.0
+Version: 2.1
 Author URI: https://github.com/dandyxu
 */
 
@@ -14,11 +14,14 @@ function smp_map_it($atts, $content=null){
 	shortcode_atts( array('title' => 'Your Map:', 'address' => ''), $atts);
 	$base_map_url = "http://maps.googleapis.com/maps/api/staticmap?center=10,+The+Crescent,+O'Connell+Street,+Limerick,+Ireland&zoom=13&scale=false&size=600x300&maptype=roadmap&key=AIzaSyAd2M4xNCXVfBanUDPM8pHvVpAJ6xgTX6M&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C10,+The+crescent,+O'Connell+Street,+Limerick,+Ireland";
 	return '
-	<h2>'. $atts['title'].'</h2>
+	<h2>'. $atts['title'].
+	do_shortcode($content) .'</h2>
 	<img width="256" height="256" 
 		src="'. $base_map_url . urlencode($atts['address']) .'" />';
 }
 
 add_shortcode('map-it', 'smp_map_it');
+//remove_all_shortcodes
+//remove_shortcode
 
 ?>
