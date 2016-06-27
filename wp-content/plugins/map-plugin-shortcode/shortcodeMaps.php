ccfb78b314cca7bd14ae4ad971075fe1af3d7f7c
dandyxu <dandyjefferson@gmail.com>
2016-06-27 16:32:46 +0100

    Day 10 Using shortcode, static map API success

--- wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
+++ wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
@@ -4,14 +4,14 @@ Plugin Name: Map plugin using shortcode
 Plugin URI: http://localhost/wpPlugin-learning/map-plugin-shortcode/
 Description: This plugin will get a map of whatever parameter is passed
 Author: Dandy Xu
-Version: 1.0
+Version: 2.0
 Author URI: https://github.com/dandyxu
 */
 
 function smp_map_it(){
 
 	$addr = "10, The Crescent, O'Connell Street, Limerick";
-	$base_map_url = "https://www.google.ie/maps/place/10+The+Crescent,+Limerick/@52.6619208,-8.6303102,15z/data=!4m2!3m1!1s0x485b5c630e53d727:0x1d6c9d51e44f8943?hl=en";
+	$base_map_url = "http://maps.googleapis.com/maps/api/staticmap?center=10,+The+Crescent,+O'Connell+Street,+Limerick,+Ireland&zoom=13&scale=false&size=600x300&maptype=roadmap&key=AIzaSyAd2M4xNCXVfBanUDPM8pHvVpAJ6xgTX6M&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C10,+The+crescent,+O'Connell+Street,+Limerick,+Ireland";
 	?>
 	<h2>Your Location: </h2>
 	<img width="256" height="256" 
@@ -19,6 +19,6 @@ function smp_map_it(){
 	<?php
 }
 
-add_shortcode ('map-it', 'smp_map_it');
+add_shortcode('map-it', 'smp_map_it');
 
 ?>
