4740fb0a7eafbac9a31aadff477452d87cb73bc1
dandyxu <dandyjefferson@gmail.com>
2016-06-27 16:43:27 +0100

    Day 10 Using shortcode, using shortcode_atts

--- wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
+++ wp-content/plugins/map-plugin-shortcode/shortcodeMaps.php
@@ -8,15 +8,15 @@ Version: 2.0
 Author URI: https://github.com/dandyxu
 */
 
-function smp_map_it(){
+function smp_map_it($atts, $content=null){
 
-	$addr = "10, The Crescent, O'Connell Street, Limerick";
+	//$addr = "10, The Crescent, O'Connell Street, Limerick";
+	shortcode_atts( array('title' => 'Your Map:', 'address' => ''), $atts);
 	$base_map_url = "http://maps.googleapis.com/maps/api/staticmap?center=10,+The+Crescent,+O'Connell+Street,+Limerick,+Ireland&zoom=13&scale=false&size=600x300&maptype=roadmap&key=AIzaSyAd2M4xNCXVfBanUDPM8pHvVpAJ6xgTX6M&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C10,+The+crescent,+O'Connell+Street,+Limerick,+Ireland";
-	?>
-	<h2>Your Location: </h2>
+	return '
+	<h2>'. $atts['title'].'</h2>
 	<img width="256" height="256" 
-		src="<?php echo $base_map_url . urlencode($addr); ?>" />
-	<?php
+		src="'. $base_map_url . urlencode($atts['address']) .'" />';
 }
 
 add_shortcode('map-it', 'smp_map_it');
