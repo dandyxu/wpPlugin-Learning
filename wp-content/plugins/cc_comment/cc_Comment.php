b11ac0073bf5bbe3af88f3588a2d7bbc7338d4e9
dandyxu <dandyjefferson@gmail.com>
2016-08-11 18:37:10 +0100

    Day 14 Using jQuery and AJAX for validate email in CC Comment Plugin

--- wp-content/plugins/cc_comment/cc_Comment.php
+++ wp-content/plugins/cc_comment/cc_Comment.php
@@ -29,6 +29,7 @@ add_action('admin_init', 'cccomm_init');
 function cccomm_setting_field(){
     ?>
     <input type="text" name="cccomm_cc_email" id="cccomm_cc_email" value="<?php echo get_option('cccomm_cc_email'); ?>" />
+    <div id="emailInfo" align="left"></div>
     <?php
 }
 
@@ -48,4 +49,28 @@ function cccomm_plugin_menu(){
 }
 add_action('admin_menu', 'cccomm_plugin_menu');
 
+/**
+ *
+ */
+function cccomm_email_check(){
+    $email = isset($_POST['cccomm_cc_email']) ? $_POST['cccomm_cc_email'] : null;
+    $msg = 'invalid';
+    if ( $email ) {
+        if  (is_email( $email) ){
+            $msg = 'valid';
+        }
+    }
+    echo $msg;
+    die();
+}
+
+add_action('wp_ajax_cccomm_email_check', 'cccomm_email_check');
+add_action('admin_print_scripts-options-general.php', 'cccomm_email_check_script');
+
+/**
+ * Load Javascript Script
+ */
+function cccomm_email_check_script(){
+    wp_enqueue_script('cc-comments', path_join(WP_PLUGIN_URL, basename( dirname(__FILE__)). "/cc_comment.js"), array('jquery'));
+}
 ?>
