368842b83955bf634f5b96969f0ffb0c700025ea
dandyxu <dandyjefferson@gmail.com>
2016-08-26 18:38:40 +0100

    Day 15 Accessing Database using wpdb 2, including User Emails, User info, All Post Terms

--- wp-content/plugins/dbinfo.php
+++ wp-content/plugins/dbinfo.php
@@ -18,6 +18,18 @@ function databaseinfo_dashboard_widget() {
     <p><b>Last Error:</b> <?php echo $wpdb->last_error; ?></p>
     <p><b>Total Users: </b><?php echo $wpdb->query('SELECT ID FROM wp_users'); ?></p>
     <p><b>Last Post: </b> <?php echo $wpdb->get_var(' SELECT post_title FROM ' . $wpdb->posts . ' WHERE post_author = ' . $current_user->ID); ?></p>
+    <p><b>User Emails: </b> <?php $user_emails = $wpdb->get_col('SELECT user_email FROM '. $wpdb->users); ?>
+        <pre><?php echo var_dump($user_emails) ?></pre>
+    </p>
+    <p><b>Your user info: </b>
+        <?php $my_user_data = $wpdb->get_row('SELECT * FROM ' . $wpdb->users . ' WHERE ID = ' .$current_user->ID, ARRAY_A); ?>
+        <pre><?php echo var_dump($my_user_data); ?></pre>
+    </p>
+
+    <p><b>All Post Terms: </b>
+        <?php $terms = $wpdb->get_results('SELECT * FROM ' . $wpdb->terms);  echo var_dump($terms); ?>
+    </p>
+
     <pre><?php //echo var_dump($wpdb); ?></pre>
 
     <?php
